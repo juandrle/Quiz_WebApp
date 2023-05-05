bef77df3a59ab7c8f97b6f9ca28fa2eaaed80956
julian andrle <julian.andrle@student.hs-rm.de>
2023-05-05 09:06:19 +0200

    fixed the save on different {frageNr} A4 complete

--- src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageController.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageController.java
@@ -1,4 +1,7 @@
 package de.hsrm.mi.web.projekt.ui.frage;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -6,21 +9,45 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes(names = { "frageFormular" })
+@SessionAttributes(names = { "formularMap" })
+@RequestMapping("/frage")
 public class FrageController {
+    final int maxfalsch = 4;
     Logger logger = LoggerFactory.getLogger(FrageController.class);
 
-    @ModelAttribute("frageFormular")
-    public void initFormular(Model m) {
-        m.addAttribute("frageFormular", new FrageFormular());
+    @ModelAttribute("formularMap")
+    public Map<String, FrageFormular> initFormularMap() {
+        return new HashMap<>();
     }
 
-    @GetMapping("/frage/{frageNr}")
-    public String getForm(Model m, @PathVariable("frageNr") String fnr) {
+    @PostMapping("/{frageNr}")
+    public String handleFormSubmit(
+            @ModelAttribute("formularMap") Map<String, FrageFormular> formularMap,
+            @PathVariable("frageNr") String fnr,
+            @ModelAttribute("frageFormular") FrageFormular formular) {
+        formularMap.put(fnr, formular);
+        return "redirect:/frage/" + fnr;
+    }
+
+    @GetMapping("/{frageNr}")
+    public String getForm(
+            Model m,
+            @PathVariable("frageNr") String fnr,
+            @ModelAttribute("formularMap") Map<String, FrageFormular> formularMap) {
+        FrageFormular formular = formularMap.get(fnr);
+        if (formular == null) {
+            formular = new FrageFormular();
+            formular.setFrage("");
+            formular.setPunkte(0);
+        }
         m.addAttribute("frageNr", fnr);
+        m.addAttribute("maxFalsch", Integer.toString(maxfalsch));
+        m.addAttribute("frageFormular", formular);
         return "fragebearbeiten";
     }
 }
