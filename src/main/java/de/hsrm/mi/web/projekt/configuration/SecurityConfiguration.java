bd23ee7614e6e237a1e0adfd8c23354f6089360f
Ilja Tkatchev <ilja.tkatchev@student.hs-rm.de>
2023-06-23 15:45:37 +0200

    Aufgabe 1 fast fertig

--- src/main/java/de/hsrm/mi/web/projekt/configuration/SecurityConfiguration.java
+++ src/main/java/de/hsrm/mi/web/projekt/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package de.hsrm.mi.web.projekt.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -24,8 +26,18 @@ public class SecurityConfiguration {
         UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
         UserDetails user1 = userBuilder.username("joenhard").password("jipi").roles("USER").build();
-        UserDetails user2 = userBuilder.username("joghurta").password("juhu").roles("USER").build();
+        UserDetails user2 = userBuilder.username("joghurta").password("juhu").roles("CHEF").build();
 
         return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+    @Bean
+    SecurityFilterChain filterChainApp(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+            .requestMatchers("/kategorie", "/frage", "quiz").permitAll()
+            .requestMatchers("/*/0", "/*/*/del").hasRole("CHEF")
+            .anyRequest().authenticated()
+        );
+        return http.build();
     }
 }
